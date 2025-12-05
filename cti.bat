94dacabdf1861197e16cabcd81932ace52466594
Gökhan SIMSEK <gokhansimsekfb@gmail.com>
2025-12-05 12:50:00 +0300

    refactoring

--- cti.bat
+++ cti.bat
@@ -4,4 +4,4 @@ setlocal enableextensions enabledelayedexpansion
 @REM Usage:
 @REM cti list       // for `src/list.cpp`
 
-clang-tidy src\%1.cpp
+clang-tidy src\%1.cpp -- -std=c++20
--- src/map.cpp
+++ src/map.cpp
@@ -1,7 +1,7 @@
 #define CATCH_CONFIG_MAIN  // Tells Catch2 to provide a main()
 #include "../catch/catch_amalgamated.hpp"
 
-#include <algorithm>
+#include <map>
 
 #include "utils.h"
 
